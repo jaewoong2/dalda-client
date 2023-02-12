a5fc0545b959479f2f936011c8de4d6f31c6ba6d
임재웅 <63512217+jaewoong2@users.noreply.github.com>
2023-02-13 08:03:02 +0900

    Update https.ts

--- src/apis/https.ts
+++ src/apis/https.ts
@@ -33,9 +33,9 @@ class Http {
   private instance: AxiosInstance | null = null
 
   // public baseURL: string = 'https://dev.dalda.shop'
-//   public baseURL: string = import.meta.env.DEV ? '' : 'https://api.dalda.shop'
-  public baseURL = ''
-  
+  // public baseURL: string = import.meta.env.DEV ? '' : 'https://api.dalda.shop'
+  public baseURL: string = ''
+
   get http(): AxiosInstance {
     return this.instance ?? this.initHttp()
   }
